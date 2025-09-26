e8e496dba9a6be435a186f3aa1591f9cfe982111
cyril <cyril.mazauric@ynov.com>
2025-09-26 10:16:09 +0200

    create http server

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Bonjour, monde !")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Serveur démarré sur http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
