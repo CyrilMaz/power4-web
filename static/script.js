93218b850eeb4abf7b58716f4fd023b6bc1e0718
ShoriMatt <matthiasrobbe34@gmail.com>
2025-10-24 09:35:04 +0200

    Fix errors

--- static/script.js
+++ static/script.js
@@ -1,22 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Toutes les cellules du plateau
   document.querySelectorAll(".cell").forEach(cell => {
     cell.addEventListener("click", event => {
-      event.preventDefault(); // ❌ empêche la redirection immédiate
+      event.preventDefault();
 
-      const url = cell.getAttribute("href"); // garde l’URL du coup
+      const url = cell.getAttribute("href");
       const plop = new Audio("/static/plop.wav");
       plop.volume = 0.6;
       plop.play();
 
-      // ✅ redirection après 150 ms (temps de jouer le son)
       setTimeout(() => {
         window.location.href = url;
       }, 500);
     });
   });
 
-  // Si un joueur a gagné → jouer son de victoire
   const status = document.getElementById("status");
   if (status && status.textContent.includes("a gagné")) {
     const win = new Audio("/static/win.wav");
